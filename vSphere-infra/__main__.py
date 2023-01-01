3bcadffa4b30b3c92eef8a44c78c3e141eedb395
nsalman <nsalman@attentigroup.com>
2023-01-01 12:52:37 +0200

    Create first resource

--- vSphere-infra/__main__.py
+++ vSphere-infra/__main__.py
@@ -1,6 +1,13 @@
 """A Python Pulumi program"""
 
 import pulumi_vsphere as vsphere
+from pulumi_vsphere import Folder
 
 datacenter = vsphere.get_datacenter(name="RnD4")
-    
+
+# Create Folder 
+
+test_folder = Folder("pulumi_resources", 
+           datacenter_id=datacenter.id,
+           path= "/pulumi-resources",
+           type="vm") 
